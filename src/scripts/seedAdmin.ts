bc80da2ec1f68b42d9e5729fe3164ed96dd08988
AsadFahimTEC <fah485434@gmail.com>
2026-01-03 11:57:43 +0600

    admin seeding part 2 added

--- src/scripts/seedAdmin.ts
+++ src/scripts/seedAdmin.ts
@@ -3,12 +3,15 @@ import { UserRole } from "../middlewares/auth";
 
 async function seedAdmin() {
     try {
+        console.log("***** Admin seeding started");
         const adminData = {
-            name: "Admin Saheb",
-            email: "admin@admin.com",
+            name: "Admin2 Saheb",
+            email: "admin2@admin.com",
             role: UserRole.ADMIN,
-            password: "admin1234"
+            password: "admin1234",
+            emailVerified: true
         }
+        console.log("***** Checking Admin Exist or not");
         // check user exist on db or not
         const existingUser = await prisma.user.findUnique({
             where: {
@@ -16,7 +19,7 @@ async function seedAdmin() {
             }
         });
 
-        if(existingUser){
+        if (existingUser) {
             throw new Error("User already exits!!");
         }
 
@@ -28,7 +31,24 @@ async function seedAdmin() {
             body: JSON.stringify(adminData)
         })
 
+
+        if (signUpAdmin.ok) {
+            console.log("***** Admin created");
+            await prisma.user.update({
+                where: {
+                    email: adminData.email
+                },
+                data: {
+                    emailVerified: true
+                }
+            })
+            console.log("***** Email vereification status updated!");
+        }
+        console.log("***** Success *****");
+
     } catch (error) {
         console.error(error);
     }
 }
+
+seedAdmin();
